df7c7d12a1e1cbf8e7dfdd47c7d9b1dc881d1b84
JonesThe4th <65411891+JonesThe4th@users.noreply.github.com>
2021-09-26 18:14:06 +0300

    Add files via upload

---
+++ EventsApp/app/src/main/java/extended/ui/eventsapp/EventsActivity.java
@@ -0,0 +1,14 @@
+package extended.ui.eventsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events_page);
+    }
+}
